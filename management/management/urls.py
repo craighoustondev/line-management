089e70a8103df7f6b047b24053b9d00a8df42da1
Craig Houston <craighouston01@gmail.com>
2018-10-14 22:50:25 +0100

    Changes to meetings edit page.

--- management/management/urls.py
+++ management/management/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meetings/', include('meetings.urls')),
+    path('', include('meetings.urls')),
 ]
 
 
